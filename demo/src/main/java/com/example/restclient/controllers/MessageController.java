78d506e9706c3aeefb5df88a8b9377baa995a99d
Hassan <hraqib1@gmail.com>
2021-11-01 15:15:12 -0400

    change to async call

--- demo/src/main/java/com/example/restclient/controllers/MessageController.java
+++ demo/src/main/java/com/example/restclient/controllers/MessageController.java
@@ -1,5 +1,12 @@
 package com.example.restclient.controllers;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.nio.file.Paths;
+import java.time.Duration;
+
 import com.example.restclient.DemoApplication;
 import com.example.restclient.models.Message;
 
@@ -10,6 +17,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.publisher.Flux;
 
 @RestController
 public class MessageController {
@@ -18,19 +28,15 @@ public class MessageController {
     private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
     
     @GetMapping(value = "/goodreads/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Message getResults(@PathVariable("id") Integer id) {
-        if(id != null) {
-
-            
-            Message message = restTemplate.getForObject("http://localhost:8080/message/"+id, Message.class);
-            log.info(message.toString());
-            return message;
-            
-		
-		}
-        return null;
-               
-
+    public Flux<Message> getMessage(@PathVariable("id") Integer id) throws URISyntaxException {
+        Flux<Message> messageFlux = WebClient.create()
+          .get()
+          .uri(new URI("http://localhost:8080/message/"+id))
+          .retrieve()
+          .bodyToFlux(Message.class);
+    
+        messageFlux.subscribe(message -> log.info(message.toString()));
+        return messageFlux;
     }
     
 }
